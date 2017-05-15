41fb55e4dda5ab72f43f7347be2f553f3eb71200
thomas <atelier@anybodesign.com>
2017-05-15 14:55:42 +0200

    Removed JS

--- js/main.js
+++ js/main.js
@@ -42,35 +42,5 @@ jQuery(document).ready(function() {
 		    }
 	});
 	
-	
-	// Scroll-Down
-	
-	
-
-	
-
-	jQuery(window).on('load',function() {
-		var scrollheight = jQuery( window ).height();
-		//console.log(''+scrollheight+'px');
-		
-		jQuery('.scroll-btn').click(function(e) {
-			e.preventDefault();		
-		
-			jQuery('body,html').stop().animate({ scrollTop: ''+scrollheight+'px' });
-		});
-	});	
-
-	jQuery(window).on('resize',function() {
-		var scrollheight = jQuery( window ).height();
-		//console.log(''+scrollheight+'px');
-		
-		jQuery('.scroll-btn').click(function(e) {
-			e.preventDefault();		
-		
-			jQuery('body,html').stop().animate({ scrollTop: ''+scrollheight+'px' });
-		});
-	});	
-
-	
 
 });

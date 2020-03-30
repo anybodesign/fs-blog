d093d7e9382bcc1ce4ffedd6f468686673e8b7a1
Thomas Villain <atelier@anybodesign.com>
2020-03-30 15:09:07 +0200

    Page banner pictures and title

--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -1,44 +1,20 @@
 <?php if ( !defined('ABSPATH') ) die(); ?>
 				
-				
-				<div class="row">
-
-					<?php if ( is_front_page() || is_search() || is_category() ) { ?>
-
-					<div class="page-banner">
+				<div class="page-banner"<?php fs_bg_img(); ?>>
 					
-					<?php } else if ( '' != get_the_post_thumbnail() && is_home() ) {
-							$large_image_url = get_the_post_thumbnail_url( get_option( 'page_for_posts' ) );
-					?>
-
-					<div class="page-banner" <?php if ( '' != get_the_post_thumbnail() ) { echo 'style="background-image: url('.$large_image_url.')"'; } ?>>
-					
-
-					<?php } else if ( '' != get_the_post_thumbnail() ) {
-							$large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' );
-					?>
-					
-					<div class="page-banner" <?php if ( '' != get_the_post_thumbnail() ) { echo 'style="background-image: url('.$large_image_url[0].')"'; } ?>>
-					
-					<?php } else { ?>
-
-					<div class="page-banner">
-					
-					<?php } ?>
-
-						
+					<div class="page-banner-title">
 						<h1 class="page-title">
 							<?php if ( is_front_page() ) { ?>
-
-							<?php if(get_theme_mod('welcome_text')) {
-								echo get_theme_mod('welcome_text', ''); 
+	
+							<?php if ( get_theme_mod('welcome_title') ) {
+								echo get_theme_mod( 'welcome_title', '' ); 
 							} else {
 								esc_html_e( 'Hello :)', 'fs-blog' ); 	
 							} ?>
 							
 							<?php } else if (is_home() ) { ?>
 							<?php single_post_title(); ?>
-
+	
 							<?php } else if (is_search() ) { ?>
 							<?php printf( esc_html__( 'Search Results for: %s', 'fs-blog' ), get_search_query() ); ?>
 							
@@ -53,7 +29,12 @@
 							
 							<?php } ?>
 						</h1>
-
-					
+	
+						<?php if ( is_front_page() || is_home() ) { ?>
+							<?php if ( get_theme_mod( 'welcome_text' ) ) { ?>
+								<p class="text-intro"><?php echo get_theme_mod('welcome_text', ''); ?></p>
+							<?php } ?>
+						<?php } ?>
 					</div>
+
 				</div>
